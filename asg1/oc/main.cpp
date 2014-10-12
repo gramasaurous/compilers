69006b608648dece85364cb284be1b13f66f0596
Graham Greving <ggreving@ucsc.edu>
2014-10-12 16:39:31 -0700

    gmake auto commit: Sun Oct 12 16:39:31 PDT 2014

---
+++ asg1/oc/main.cpp
@@ -0,0 +1,12 @@
+// Graham Greving
+// ggreving@ucsc.edu
+// cmps104a: fall 2014
+// asg1: stringset + preprocessor
+
+// #include statements
+#include "debug.h"
+#include "util.h"
+
+int main (int argc, char **argv) {
+   
+}
